485ee30fd110b35d05868b3071215859cf2b1050
zrt <xsfzrt@gmail.com>
2019-05-15 22:07:18 +0800

    update code

--- server/main.go
+++ server/main.go
@@ -1,12 +1,21 @@
 package main
 import(
     "io"
+    "io/ioutil"
     "net/http"
 )
 func main(){
     main_handler := func(w http.ResponseWriter, req *http.Request){
-        io.WriteString(w, "hello,world")
+        if content, err := ioutil.ReadFile("main.html"); err != nil{
+            io.WriteString(w, "read main.html error")
+        }else{
+            w.Write(content)
+        }
     }
-    http.HandleFunc("/hello", main_handler)
+    http.Handle("/css/", http.FileServer(http.Dir("./")))
+    http.Handle("/js/", http.FileServer(http.Dir("./")))
+    http.Handle("/html/", http.FileServer(http.Dir("./")))
+    http.Handle("/video/", http.FileServer(http.Dir("./")))
+    http.HandleFunc("/", main_handler)
     http.ListenAndServe(":8080", nil)
 }
